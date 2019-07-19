56554cb58cd890c35f591f4487e334f3581c2aeb
john <filip.icev@digipen.edu>
2019-07-19 11:31:18 -0700

    Auto stash before merge of "master" and "origin/master"
    i added the platforms

--- src/main.cpp
+++ src/main.cpp
@@ -12,30 +12,13 @@
 #include "Debug.hpp"
 #include <iostream>
 
-
+// This is my special comment
 
 ///Goals for Elle://///
 //-Make background sprite the background of the game
 //
 
-
-
-
-
-
-
-
-
-
-
-
-
 ////// PUT PLAYER CODE IN GROUP GITHUB //////
-
-
-
-
-
 
 using Aspen::Engine::Engine;
 using Aspen::GameState::GameState;
@@ -43,19 +26,17 @@ using Aspen::GameState::GameStateManager;
 using Aspen::Graphics::Graphics;
 using Aspen::Object::Object;
 
-
 void ChangeScene(Aspen::Graphics::UI::Button *button, std::string scene, GameStateManager *gsm)
 {
   gsm->SetCurrentState(scene);
 }
 
-
-  class Player : public Aspen::Graphics::Animation
+class Player : public Aspen::Graphics::Animation
 {
 public:
   Player(Object *parent = nullptr, std::string name = "Player") : Aspen::Graphics::Animation(
-                                                                        new Aspen::Graphics::UniformSpritesheet("./resources/lundmarkyeezy.png", 300, 300, 1, nullptr, "Loadng"),
-                                                                        1.0f / 12.0f, parent, name)
+                                                                      new Aspen::Graphics::UniformSpritesheet("./resources/lundmarkyeezy.png", 300, 300, 1, nullptr, "Loadng"),
+                                                                      1.0f / 12.0f, parent, name)
 
   {
     GetTransform()->SetPosition(200, 200);
@@ -64,7 +45,7 @@ public:
     CreateChild<Aspen::Physics::Rigidbody>();
   }
 };
-  class Player2 : public Aspen::Graphics::Animation
+class Player2 : public Aspen::Graphics::Animation
 {
 public:
   Player2(Object *parent = nullptr, std::string name = "Player2") : Aspen::Graphics::Animation(
@@ -81,232 +62,199 @@ public:
 
 class FO : public Aspen::Object::Object
 {
-bool GoingRight = true;
+  bool GoingRight = true;
+
 public:
-  FO(Object* parent = nullptr, std::string name = "Enemy") : Aspen::Object::Object (parent, name)
+  FO(Object *parent = nullptr, std::string name = "Enemy") : Aspen::Object::Object(parent, name)
   {
     CreateChild<Aspen::Transform::Transform>();
     CreateChild<Aspen::Physics::Rigidbody>();
-    CreateChild<Aspen::Physics::AABBCollider>()->SetSize(300,100);  
+    CreateChild<Aspen::Physics::AABBCollider>()->SetSize(300, 100);
   }
   void OnUpdate()
   {
-      if (GoingRight)
-      {
-        GetRigidbody()->SetCartesianVelocity(3,0);
-      }
-      if (!GoingRight)
-      {
-        GetRigidbody()->SetCartesianVelocity(-3,0);
-      }
+    if (GoingRight)
+    {
+      GetRigidbody()->SetCartesianVelocity(3, 0);
+    }
+    if (!GoingRight)
+    {
+      GetRigidbody()->SetCartesianVelocity(-3, 0);
+    }
   }
 };
 
 class Enemy : public Aspen::Object::Object
 {
-Aspen::Graphics::Animation anim1;
-bool GoingRight = true;
+  Aspen::Graphics::Animation anim1;
+  bool GoingRight = true;
 
 public:
-Enemy(Object* parent = nullptr, std::string name = "Enemy") : Aspen::Object::Object (parent, name)
-{
-CreateChild<Aspen::Transform::Transform>();
-CreateChild<Aspen::Physics::Rigidbody>();
-CreateChild<Aspen::Physics::AABBCollider>()->SetSize(100,100);
-}
+  Enemy(Object *parent = nullptr, std::string name = "Enemy") : Aspen::Object::Object(parent, name)
+  {
+    CreateChild<Aspen::Transform::Transform>();
+    CreateChild<Aspen::Physics::Rigidbody>();
+    CreateChild<Aspen::Physics::AABBCollider>()->SetSize(100, 100);
+  }
 
-void OnUpdate()
-{
-if (GoingRight)
-{
-  GetRigidbody()->SetCartesianVelocity(3,0);
-}
-if (!GoingRight)
-{
-  GetRigidbody()->SetCartesianVelocity(-3,0);
-}
-}
-void OnCollision(Aspen::Physics::Collision c)
-{
-GoingRight = !GoingRight;
-}
+  void OnUpdate()
+  {
+    if (GoingRight)
+    {
+      GetRigidbody()->SetCartesianVelocity(3, 0);
+    }
+    if (!GoingRight)
+    {
+      GetRigidbody()->SetCartesianVelocity(-3, 0);
+    }
+  }
+  void OnCollision(Aspen::Physics::Collision c)
+  {
+    GoingRight = !GoingRight;
+  }
 };
-
-
 
 class Platform : public Aspen::Graphics::Rectangle
-{ 
+{
 public:
-  Platform(Object *parent = nullptr, std::string name = "Platform") :
-  Aspen::Graphics::Rectangle(
-    SDL_Rect ({0,0,32, 32}),
-    Aspen::Graphics::Colors::RED, true, parent, name)
-    {
-      GetTransform()->SetPosition(100, 200);
-      CreateChild<Aspen::Physics::AABBCollider>()->SetSize(32, 32);
-      CreateChild<Aspen::Physics::Rigidbody>();
-
-    }
-      
-      void OnCollision (Aspen::Physics::Collision c)
-    {
-
-    Aspen::Log::Debug("COLLISION");
-    
-    }
-
+  Platform(Object *parent = nullptr, std::string name = "Platform") : Aspen::Graphics::Rectangle(
+                                                                          SDL_Rect({0, 0, 2600, 60}),
+                                                                          Aspen::Graphics::Colors::RED, true, parent, name)
+  {
+    GetTransform()->SetPosition(640, 60);
+    CreateChild<Aspen::Physics::AABBCollider>()->SetSize(2600, 60);
+  }
 };
-
-
 
 class Background : public Aspen::Object::Object
 
 {
-Aspen::Graphics::Sprite *BG;  
+  Aspen::Graphics::Sprite *BG;
+
 public:
+  Background(Object *parent = nullptr, std::string name = "Background") :
 
-Background(Object *parent = nullptr, std::string name = "Background") :
-
-  Object(parent, name)
+                                                                          Object(parent, name)
   {
     CreateChild<Aspen::Transform::Transform>();
     BG = new Aspen::Graphics::Sprite("./resources/Background_Final.png", parent, "background");
-    AddChild(BG); 
+    AddChild(BG);
   }
-
-
-
-
-
-
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 class MainMenu : public GameState
 {
-  
-  Aspen::Graphics::UI::Text  *title;
-  
+
+  Aspen::Graphics::UI::Text *title;
+
   Player *player;
-  Platform *platform;
+
   Enemy *enemy1;
   Player2 *player2;
-  
+  Platform *platform;
+  Platform *platform2;
+  Platform *platform3;
+
 public:
-  MainMenu(Object *parent=nullptr, std::string name="MainMenu") : GameState(parent, name)
-  
-  
+  MainMenu(Object *parent = nullptr, std::string name = "MainMenu") : GameState(parent, name)
+
   {
-  
-  Background *BG = new Background(nullptr, "Background");
-  //Aspen::Log::Debug("Background is in the program!")
-  BG->GetTransform()->SetPosition(200, 200);
-  AddChild(BG);
-  title = new Aspen::Graphics::UI::Text("My Cool Game" , "default", 64, this, "Title");
-  AddChild(title);
-  title->GetTransform()->SetPosition( 271, 117);
-  title->GetTransform()->SetRotation( 3.128f );
-  title->GetTransform()->SetScale( 1, 1);
 
- 
+    Background *BG = new Background(nullptr, "Background");
+    //Aspen::Log::Debug("Background is in the program!")
+    BG->GetTransform()->SetPosition(200, 200);
+    AddChild(BG);
+    title = new Aspen::Graphics::UI::Text("My Cool Game", "default", 64, this, "Title");
+    AddChild(title);
+    title->GetTransform()->SetPosition(271, 117);
+    title->GetTransform()->SetRotation(3.128f);
+    title->GetTransform()->SetScale(1, 1);
 
-  enemy1 = new Enemy();
-  AddChild(enemy1);
+    enemy1 = new Enemy();
+    AddChild(enemy1);
 
-
-
-
-  player = new Player();
-  AddChild(player);
-  platform = new Platform(); 
-  AddChild(platform); 
-  player2 = new Player2();
-  AddChild(player2);
+    player = new Player();
+    AddChild(player);
+    player2 = new Player2();
+    AddChild(player2);
+    platform = new Platform();
+    AddChild(platform);
+    platform->GetTransform()->SetPosition(640, 670);
+    platform2 = new Platform();
+    AddChild(platform2);
+    platform2->GetTransform()->SetPosition(640, 400);
+    platform3 = new Platform();
+    AddChild(platform3);
+    platform3->GetTransform()->SetPosition(640, 400);
   }
-  
- 
 
   void OnUpdate()
   {
-   
- if (Aspen::Input::KeyHeld(SDLK_UP))
+
+    if (Aspen::Input::KeyHeld(SDLK_UP))
     {
       player2->GetRigidbody()->SetCartesianVelocity(0, 5);
     }
-    
+
     if (Aspen::Input::KeyHeld(SDLK_RIGHT))
     {
-      player2->GetRigidbody()->SetCartesianVelocity(5, 0 );
+      player2->GetRigidbody()->SetCartesianVelocity(5, 0);
     }
     if (Aspen::Input::KeyHeld(SDLK_LEFT))
     {
       player2->GetRigidbody()->SetCartesianVelocity(-5, 0);
     }
-      
-    
-       if (Aspen::Input::KeyHeld(SDLK_w))
-      {
-         player->GetRigidbody()->SetCartesianVelocity(0, 5);
-      }
-       
-      if (Aspen::Input::KeyHeld(SDLK_a))
-      {
-         player->GetRigidbody()->SetCartesianVelocity(-5, 0);
-        
-      }
-      if (Aspen::Input::KeyHeld(SDLK_d))
-      {
-        player->GetRigidbody()->SetCartesianVelocity( 5, 0);
-      }
 
-      
+    if (Aspen::Input::KeyHeld(SDLK_w))
+    {
+      player->GetRigidbody()->SetCartesianVelocity(0, 5);
+    }
 
-   
+    if (Aspen::Input::KeyHeld(SDLK_a))
+    {
+      player->GetRigidbody()->SetCartesianVelocity(-5, 0);
+    }
+    if (Aspen::Input::KeyHeld(SDLK_d))
+    {
+      player->GetRigidbody()->SetCartesianVelocity(5, 0);
+    }
+
+    if (Aspen::Input::KeyHeld(SDLK_w))
+    {
+      player->GetRigidbody()->SetCartesianVelocity(0, 5);
+    }
+
+    if (Aspen::Input::KeyHeld(SDLK_a))
+    {
+      player->GetRigidbody()->SetCartesianVelocity(-5, 0);
+    }
+    if (Aspen::Input::KeyHeld(SDLK_d))
+    {
+      player->GetRigidbody()->SetCartesianVelocity(5, 0);
+    }
+
     //if (title->GetTransform()-> SetScale(1, 1));
 
-
-   // Aspen::Log::Debug("%d", Aspen::Graphics::DEFAULT_WINDOW_WIDTH);
+    // Aspen::Log::Debug("%d", Aspen::Graphics::DEFAULT_WINDOW_WIDTH);
   }
-
-
 };
-
 
 class Game : public GameState
 {
-  
 };
- 
 
 int main(int argc, char **argv)
 {
   Aspen::Log::Log::SetFile("./Aspen.log");
 
-
-  Engine engine(Aspen::Engine::START_FLAGS::ALL ^ (
-    Aspen::Engine::START_FLAGS::CREATE_GRAPHICS |
-    Aspen::Engine::START_FLAGS::CREATE_GRAPHICS_DEBUGGER |
-    Aspen::Engine::START_FLAGS::CREATE_GRAPHICS_FONTCACHE
-    ));
+  Engine engine(Aspen::Engine::START_FLAGS::ALL ^ (Aspen::Engine::START_FLAGS::CREATE_GRAPHICS |
+                                                   Aspen::Engine::START_FLAGS::CREATE_GRAPHICS_DEBUGGER |
+                                                   Aspen::Engine::START_FLAGS::CREATE_GRAPHICS_FONTCACHE));
   Aspen::Graphics::Graphics *gfx = new Aspen::Graphics::Graphics(1280, 720, &engine, "Graphics 720p");
   engine.AddChild(gfx);
   gfx->CreateChild<Aspen::Debug::Debug>();
-  gfx->CreateChild<Aspen::Graphics::FontCache>(); 
-  
+  gfx->CreateChild<Aspen::Graphics::FontCache>();
+
   engine.FindChildOfType<Aspen::Physics::Physics>()->SetGravityStrength(0);
   engine.FindChildOfType<Aspen::Physics::Physics>()->SetDrag(0.1);
   engine.FindChildOfType<Aspen::Time::Time>()->TargetFramerate(60);
@@ -319,10 +267,6 @@ int main(int argc, char **argv)
     engine();
   return 0;
 }
-
-
-
-
 
 ////////////  W:640   H:480 //////////
 
